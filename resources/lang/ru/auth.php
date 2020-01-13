7631832d97579ada26f18783323a9bf2b89745aa
Alexander Mazalov <fel1kssx@gmail.com>
2020-01-14 00:12:37 +0300

    init

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+
+	
+	'failed' => 'Неверные учетные данные.',
+	'throttle' => 'Слишком много попыток входа в систему. Пожалуйста, повторите попытку через: секунды, секунды. ',
+	'login' => 'Логин',
+	'logout' => 'Выйти',
+	'register' => "Регистрация",
+	'remember_me' => 'Запомнить меня',
+	'forgotten_password' => 'Забыли пароль?',
+	'reset_password' => 'Сбросить пароль',
+	'send_password_reset_link' => 'Отправить ссылку для сброса пароля',
+	'logged_in' => 'Вы вошли в систему',
+	'logged_in_provider' => 'Вы вошли в систему с помощью: Provider.',
+	'not_authorized' => "Не авторизован.",
+
+	'services' => [
+         'github' => 'Войти через Github',
+         'twitter' => 'Войти через Twitter',
+     ],
+
+];
---
+++ resources/lang/ru/users.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+	'profile' => 'Мой профиль',
+	'public_profile' => 'Мой общедоступный профиль',
+	'settings' => 'Настройки',
+	'edit' => 'Редактировать профиль',
+	'show' => 'Показать профиль',
+	'updated' => 'Профиль обновлен',
+	'password_updated' => 'Пароль обновлен',
+	'new_users' => 'Новый пользователь | Новые пользователи',
+	'count' => ': count user |: count users',
+	'api_token' => "Ключ API",
+	'security' => "Безопасность",
+	
+	'attribute' => [
+		'name' => 'Имя',
+		'email' => 'Email',
+		'current_password' => 'Текущий пароль',
+		'password' => 'Password',
+		'password_confirmation' => 'Подтверждение пароля',
+		'role' => 'Роли',
+		'istered_at '=>' Зарегистрирован в ',
+		'api_token' => "Ключ API"
+	],
+	
+	'placeholder' => [
+		'name' => 'Ваше имя',
+		'email' => 'Ваш электронный адрес',
+		'current_password' => 'Ваш текущий пароль',
+		'password' => 'Ваш новый пароль',
+		'password_confirmation' => 'Подтверждение пароля'
+	]
+];
